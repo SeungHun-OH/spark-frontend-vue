f3bc9443cb4b4761540f0ce0f7feacdfc46284b4
SeungHun-OH <oh_0209@naver.com>
2025-09-19 10:07:06 +0900

    vue 초기 세팅

---
+++ src/views/AboutView.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+
+// 반응형 상태
+const count = ref(0)
+
+// 상태를 변경하고 업데이트를 트리거하는 함수
+function increment() {
+  count.value++
+}
+
+// 라이프사이클 훅
+onMounted(() => {
+  console.log(`The initial count is ${count.value}.`)
+})
+</script>
+
+<template>
+  <button @click="increment">Count is: {{ count }}</button>
+</template>
